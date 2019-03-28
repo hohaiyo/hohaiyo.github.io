fa040523506cbe05660ecce207c9a5f5147018e0
LBW6842 <46664827+LBW6842@users.noreply.github.com>
2019-03-28 20:09:12 +0800

    Delete usually.js

--- 三阶段考核/jQuery/usually.js
+++
@@ -1,9 +0,0 @@
-	function choose_index(btn){
-			var yes = btn.getElementsByTagName('img')[0];
-			if(yes.style.display == 'none'||yes.style.display == ''){
-				yes.style.display = 'block';
-			}
-			else{
-				yes.style.display = 'none';
-			}
-		}
