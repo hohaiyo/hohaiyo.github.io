793a3776b981def14de8f308f36fd206d9de292b
xwt <1721152358@qq.com>
2019-04-03 19:22:18 +0800

    完善移动端头部和尾部

--- jQuery/header-footer.js
+++ jQuery/header-footer.js
@@ -13,4 +13,9 @@ $(function () {
         });
         $(this).removeClass("hover");
     });
+
+    // 使底部logo居中
+    if($(window).width()<767){
+        $(".footer_warp").children(".pull-left").addClass("center-block").removeClass("pull-left")
+    }
 });
--- jQuery/moveDom.js
+++ jQuery/moveDom.js
@@ -1,4 +1,5 @@
 $(function(){
+	
 	if($(window).width()<770){
 		var preDom=$(".problem_list>ul>li>div>img");
 		var nextDom=$(".problem_list>ul>li>div[class='col-md-9']")
