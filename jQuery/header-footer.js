29a739626b32673893209a193cc70db958fbe07c
hohaiyo <2407513675@qq.com>
2019-04-03 19:43:31 +0800

    merge xwt done

--- jQuery/header-footer.js
+++ jQuery/header-footer.js
@@ -1,5 +1,4 @@
 $(function () {
-<<<<<<< HEAD
 	$(window).scroll(function(){
 		$(".anchor").css("top",($(window).height() - $('.anchor').outerHeight())/2 + $(document).scrollTop())
 	})
@@ -20,26 +19,8 @@ $(function () {
 		});
 		$(this).removeClass("hover");
 	});
-})
-=======
-    $(".anchor li").mouseover(function () {
-        $(this).css({
-            left:'-100px',
-            backgroundColor: "rgba(0,0,0,0.7)"
-        });
-        $(this).addClass("hover");
-    })
-    $(".anchor li").mouseout(function(){
-        $(this).css({
-            left:"0",
-            backgroundColor: "rgba(0,0,0,0.3)"
-        });
-        $(this).removeClass("hover");
-    });
-
-    // 使底部logo居中
-    if($(window).width()<767){
+	 // 使底部logo居中
+	 if($(window).width()<767){
         $(".footer_warp").children(".pull-left").addClass("center-block").removeClass("pull-left")
     }
-});
->>>>>>> xwt
+})
