b46b55e398284a6c6b05c46bc6847f1c0006c017
zzt <1241700385@qq.com>
2019-04-07 23:23:12 +0800

    完成版

--- jQuery/authentication.js
+++ jQuery/authentication.js
@@ -1,95 +1,16 @@
 $(function(){
-    //下拉菜单
-    $('.user-top').click(
-        function () {
-            $('.dropdown').toggle();
+    $(window).mousemove(function () {
+        if($("#sfzname").val().length >= 2){
+            $("#ty").removeAttr("disabled").removeClass("send-disabled");
+        }else{
+            $("#ty").attr( "disabled","disabled" ).addClass( "send-disabled" );
         }
-    )
-    //头像hover效果
-    $('#headIcon').hover(
-        function () {
-            $('#iconimg').css('display', 'block');
-        }, function () {
-            $('#iconimg').css('display', 'none');
+    });
+    $(window).mousemove(function () {
+        if($("#sfzsz").val().length == 18){
+            $("#ty").removeAttr("disabled").removeClass("send-disabled");
+        }else{
+            $("#ty").attr( "disabled","disabled" ).addClass( "send-disabled" );
         }
-    );
-    //验证码
-    // $(window).mousemove(function () {
-    //     if ($(".form-number > input").val().length > 11 && $(".send").text() == "发送验证码") {
-    //         $(".send").removeAttr("disabled").removeClass("send-disabled")
-    //     } else {
-    //         $(".send").attr("disabled", "disabled").addClass("send-disabled");
-    //     }
-    // });
-
-    //验证码点击
-    $(".send").click(function () {
-        var num = 60;
-        send(num);
     });
-    var timer = null;
-    function send(a) {
-        var num = a--;
-        $(".send").text("重新发送(" + a + "s)").css("color");
-        $(".send").attr("disabled", "disabled").addClass("send-disabled");
-        clearInterval(timer);
-        timer = setInterval(function () {
-            if (a == 0) {
-                clearInterval(timer);
-                var time = null;
-                clearTimeout(time);
-                time = setTimeout(function () { $(".send").text("发送验证码") }, 5000);
-            } else {
-                send(a)
-            }
-        }, 1000);
-    };
-    //修改按钮de点击效果
-    $('#quxiao').click(function () {
-        $('.form-content').css('display', 'none');
-        $('#item-content-id').css('display', 'block');
-    });
-    $('#queding').click(function () {
-        $('.form-content').css('display', 'none');
-        $('#item-content-id').css('display', 'block');
-    });
-    $('.xiugai').click(function () {
-        $('.form-content').css('display', 'block');
-        $('#item-content-id').css('display', 'none');
-    });
-    //个人资料的消失隐藏
-    $('.teamxiugai').click(function () {
-        $('#personform-now').css('display', 'none');
-        $('#personform-set').css('display', 'block');
-    })
-    $('.team-set-btn').click(function () {
-        $('#personform-now').css('display', 'block');
-        $('#personform-set').css('display', 'none');
-    })
-    $('.team-set-btn-quxiao').click(function () {
-        $('#personform-now').css('display', 'block');
-        $('#personform-set').css('display', 'none');
-    })
-    //实名认证的禁止点击按钮
-        var Idcard = $('#input-idcard');
-        var Idnum = $('#input-idnum');
-        var btn = $('#mian-form-submit');
-        
-        $(Idnum).hover(function(){
-            var Val = $('#input-idnum').val();
-            var Idcard = $('#input-idcard').val();
-            if(Val.length == 18 && Idcard.length !== ''){
-                $(btn).addClass('pointer').removeClass('not-allowed').removeAttr('disabled');
-            }else{
-                $(btn).removeClass('pointer').addClass('not-allowed');
-            } 
-        },function(){
-            var Val = $('#input-idnum').val();
-            var Idcard = $('#input-idcard').val();
-            if(Val.length == 18 && Idcard.length !== ''){
-                $(btn).addClass('pointer').removeClass('not-allowed').removeAttr('disabled');
-            }else{
-                $(btn).removeClass('pointer').addClass('not-allowed');
-            }
-        })
 })
--- jQuery/userinfo.js
+++ jQuery/userinfo.js
@@ -7,4 +7,103 @@ $(function(){
     $('#panel-body-img').click(function(){
         $('#panel-body-file-two').click();
     })
+    //邮箱验证码
+    $(window).mousemove(function () {
+        var Ema = $("#email").val();
+        var Email = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+        if(Email.test(Ema)){
+            $(".send").removeAttr("disabled").removeClass("send-disabled");
+        }else{
+            $(".send").attr( "disabled","disabled" ).addClass( "send-disabled" );
+        }
+    });
+    //手机验证码
+    $(window).mousemove(function () {
+        if($(".phone").val().length == 11){
+            $("#send2").removeAttr("disabled").removeClass("send-disabled");
+        }else{
+            $("#send2").attr( "disabled","disabled" ).addClass( "send-disabled" );
+        }
+    });
+    //邮箱验证码点击
+    $(".send").click(function () {
+        var num = 60;
+        send(num);
+    });
+    var timer = null;
+    function send(a){
+        var num = a--;
+        $(".send").text("重新发送("+a+"s)").css("color",)
+        clearInterval(timer);
+        timer = setInterval(function(){
+            if(a == 0){
+                clearInterval(timer);
+                var time = null;
+                clearTimeout(time);
+                time = setTimeout(function(){ $(".send").text("发送验证码") },5000);
+            }else{
+                send(a)
+            }
+        },1000);
+    };
+    //手机验证码点击
+    $("#send2").click(function () {
+        var num = 60;
+        send(num);
+    });
+    var timer = null;
+    function send(a){
+        var num = a--;
+        $("#send2").text("重新发送("+a+"s)").css("color",)
+        clearInterval(timer);
+        timer = setInterval(function(){
+            if(a == 0){
+                clearInterval(timer);
+                var time = null;
+                clearTimeout(time);
+                time = setTimeout(function(){ $("#send2").text("发送验证码") },5000);
+            }else{
+                send(a)
+            }
+        },1000);
+    };
+    //点击修改出现和隐藏--邮箱
+    $('#xiugai').click(function(){
+        $('#you').addClass('none-w').removeClass('block-y');
+        $('#wu').addClass('block-y').removeClass('none-w');
+    })
+    $('#quxiao').click(function(){
+        $('#wu').addClass('none-w').removeClass('block-y');
+        $('#you').addClass('block-y').removeClass('none-w');
+    })
+    $('#queding').click(function(){
+        $('#wu').addClass('none-w').removeClass('block-y');
+        $('#you').addClass('block-y').removeClass('none-w');
+    })
+    //点击修改出现和隐藏--手机号
+    $('#xiugai2').click(function(){
+        $('#you2').addClass('none-w').removeClass('block-y');
+        $('#wu2').addClass('block-y').removeClass('none-w');
+    })
+    $('#quxiao2').click(function(){
+        $('#wu2').addClass('none-w').removeClass('block-y');
+        $('#you2').addClass('block-y').removeClass('none-w');
+    })
+    $('#queding2').click(function(){
+        $('#wu2').addClass('none-w').removeClass('block-y');
+        $('#you2').addClass('block-y').removeClass('none-w');
+    })
+    //个人资料的隐藏出现效果
+    $('.xiugai3').click(function(){
+        $('#you3').addClass('none-w').removeClass('block-y');
+        $('#wu3').addClass('block-y').removeClass('none-w');
+    })
+    $('.team-set-btn-quxiao').click(function(){
+        $('#wu3').addClass('none-w').removeClass('block-y');
+        $('#you3').addClass('block-y').removeClass('none-w');
+    })
+    $('.team-set-btn').click(function(){
+        $('#wu3').addClass('none-w').removeClass('block-y');
+        $('#you3').addClass('block-y').removeClass('none-w');
+    })
 })
