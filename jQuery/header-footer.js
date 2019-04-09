47626e672903368d798edcb3fd24eb9b38364a09
hohaiyo <2407513675@qq.com>
2019-04-09 09:14:15 +0800

    updata all

--- jQuery/header-footer.js
+++ jQuery/header-footer.js
@@ -5,7 +5,6 @@ $(function () {
         $(".navbar-nav").css("display","block")
     });
 
-<<<<<<< HEAD
     //隐藏搜索框
     $(".nav-oneself").on("click",function(){
         if($(window).width() < 768 && $(".dropdown-menu").css("display") == "none"){
@@ -49,35 +48,3 @@ $(function () {
     }
 
 });
-=======
-	$(".anchor li").mouseover(function () {
-		$(this).css({
-			left:'-100px',
-			width: "150px",
-			backgroundColor: "rgba(0,0,0,0.7)"
-		});
-		$(this).addClass("hover");
-	});
-	$(".anchor li").mouseout(function(){
-		$(this).css({
-			left:"0",
-			width: "50px",
-			backgroundColor: "rgba(0,0,0,0.3)"
-		});
-		$(this).removeClass("hover");
-	});
-	$(".navbar-nav").click(function(){
-        if($(window).width()<504){
-            if($(".seek").css("display") == "block"){
-                $(".seek").css("display","none")
-            }else{
-                $(".seek").css("display","block")
-            }
-            $(window).click(function(){
-                 $(".seek").css("display","block")
-            })
-        }
-        
-    })
-});
->>>>>>> yp
