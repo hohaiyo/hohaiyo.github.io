c76aae51782f47ef2a6375be954c755807d9b59c
ykm-nb <1621970846@qq.com>
2019-04-11 23:47:23 +0800

    12

--- jQuery/published-project.js
+++ jQuery/published-project.js
@@ -31,7 +31,7 @@ $(function(){
                         </p>
                         <p class="bottom-money">
                             <span>金额</span>
-                            <span class="bottom-gray">￥1,111</span>
+                            <span class="bottom-gray bottom-price">￥${(data.data)[i].price}</span>
                             <span>周期</span>
                             <span class="bottom-gray">11天</span>
                         </p>
@@ -44,79 +44,83 @@ $(function(){
             </div>`;
             $(".container").append(stx)
         });
-        //转换状态
+
+        //转换文字状态
         $(".list-status").each(function () {
             if($(this).text() == "1"){
-                $(".list-status").text("招标中");
+                $(".list-status").text("开发中").addClass("list-develop");
             };
         });
-    });
-
-
-
-
-
-
-    //所有状态的点击
-    $(".form-pulldown li").click(function () {
-        $(".form-status span").text($(this).text())
-    });
-
-var _this;
-var recruit;
-    // 点击取消弹出窗口
-    $(".btn-cancel").click(function () {
-        // 存储当前取消发布按钮
-        _this = $(this);
-        //当前按钮所对应的 list-status
-        recruit = $(this).parent().parent().parent().prev().find(".list-status");
-        // 禁用所有取消发布按钮
-        $(".btn-cancel").addClass("btn-disabled");
-        //当前按钮的弹出窗口
-        $(".cancel-alert").css("display","block").animate({ opacity: 1 },500);
-        // 点击关闭的i标签
-        $(".alert-close").click(function(){
-            $(".cancel-alert").css("display","none");
-            // 开启所有取消发布按钮
-            $(".btn-cancel").removeClass("btn-disabled");
+        //转换金额
+        $(".bottom-price").each(function () {
+            var _this = $(this).text().split('').reverse();
+            _this.splice(3, 0, ",");
+            $(this).text(_this.reverse().join(''));
         });
-        // yse按钮
-        $(".alert-yes").click(function () {
-            recruit.text("已取消").attr("class","list-status");
-            $(".cancel-alert").css("display","none");
-            // 去除当前的取消发布按钮和编辑按钮
-            _this.css('display','none');
-            // 开启所有取消发布按钮
-            $(".btn-cancel").removeClass("btn-disabled");
-        });
-        // no按钮
-        $(".alert-no").click(function () {
-            $(".cancel-alert").css("display","none");
-            // 开启所有取消发布按钮
-            $(".btn-cancel").removeClass("btn-disabled");
-        });
-    });
 
-    // 发布新的项目
-    $(".btn-publish").on("click", function () {
-        location.href = "demand.html";
-    });
-    // 项目状态
-    $(".btn-examine").on("click", function () {
-        var _this = $(this).parent().parent().parent().parent().find(".list-status").text();
-        if(_this === "开发中"){
-            location.href = "project-status.html";
-        }else if(_this === "已取消"){
-            location.href = "own-cancel.html";
-        }
-    });
-    // 项目详情
-    $(".list-bottom > img").on("click", function () {
-        var _this = $(this).parent().parent().find(".list-status").text();
-        if(_this === "招标中"){
-            location.href = "project_details.html";
-        }else if(_this === "已取消"){
-            location.href = "cancel-condition.html";
-        }
+        //所有状态的点击
+        $(".form-pulldown li").click(function () {
+            $(".form-status span").text($(this).text())
+        });
+
+
+
+        var _this;
+        var recruit;
+        // 点击取消弹出窗口
+        $(".btn-cancel").click(function () {
+            // 存储当前取消发布按钮
+            _this = $(this);
+            //当前按钮所对应的 list-status
+            recruit = $(this).parent().parent().parent().prev().find(".list-status");
+            // 禁用所有取消发布按钮
+            $(".btn-cancel").addClass("btn-disabled");
+            //当前按钮的弹出窗口
+            $(".cancel-alert").css("display","block").animate({ opacity: 1 },500);
+            // 点击关闭的i标签
+            $(".alert-close").click(function(){
+                $(".cancel-alert").css("display","none");
+                // 开启所有取消发布按钮
+                $(".btn-cancel").removeClass("btn-disabled");
+            });
+            // yse按钮
+            $(".alert-yes").click(function () {
+                recruit.text("已取消").attr("class","list-status");
+                $(".cancel-alert").css("display","none");
+                // 去除当前的取消发布按钮和编辑按钮
+                _this.css('display','none');
+                // 开启所有取消发布按钮
+                $(".btn-cancel").removeClass("btn-disabled");
+            });
+            // no按钮
+            $(".alert-no").click(function () {
+                $(".cancel-alert").css("display","none");
+                // 开启所有取消发布按钮
+                $(".btn-cancel").removeClass("btn-disabled");
+            });
+        });
+
+        // 发布新的项目
+        $(".btn-publish").on("click", function () {
+            location.href = "demand.html";
+        });
+        // 项目状态
+        $(".btn-examine").on("click", function () {
+            var _this = $(this).parent().parent().parent().parent().find(".list-status").text();
+            if(_this === "开发中"){
+                location.href = "project-status.html";
+            }else if(_this === "已取消"){
+                location.href = "own-cancel.html";
+            }
+        });
+        // 项目详情
+        $(".list-bottom > img").on("click", function () {
+            var _this = $(this).parent().parent().find(".list-status").text();
+            if(_this === "招标中"){
+                location.href = "project_details.html";
+            }else if(_this === "已取消"){
+                location.href = "cancel-condition.html";
+            }
+        });
     });
 });
